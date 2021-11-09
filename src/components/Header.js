b71ce052b92bd37542a6ae86ec481916d5b3a773
mmsesay <maej_01@hotmail.com>
2021-11-09 11:10:09 +0000

    Add: Space icon to the header Add: style for the icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import usePathname from '../utils/usePathName';
-
+import logo from '../images/logo.png';
 import './App.css';
 
 export default function Header() {
-  const { pathname } = usePathname();
-  console.log(pathname);
   return (
     <Navbar bg="white" expand="lg" className="border border-bottom-3">
       <Container className="p-3 bg-white">
-        <NavLink to="/" className="h2 text-decoration-none text-dark me-5 fw-bold">Space Traveler&apos;s Hub</NavLink>
+        <NavLink to="/" className="h2 text-decoration-none text-dark me-5 fw-bold">
+          <img src={logo} alt="logo" className="logo" />
+          Space Traveler&apos;s Hub
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav
             className="ms-auto m-2 my-lg-0"
             navbarScroll
           >
-            <NavLink to="/" className="h5 me-4 link text-decoration-none" activeClassName="link-active">Rockets</NavLink>
-            <NavLink to="missions" className="h5 me-4 link text-decoration-none" activeClassName="link-active">Missions</NavLink>
-            <NavLink to="profile" className="h5 me-4 link text-decoration-none" activeClassName="link-active">My Profile</NavLink>
+            <Nav.Link to="/" className="h5 me-4 link text-decoration-none">Rockets</Nav.Link>
+            <Nav.Link to="missions" className="h5 me-4 link text-decoration-none">Missions</Nav.Link>
+            <Nav.Link to="profile" className="h5 me-4 link text-decoration-none">My Profile</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
