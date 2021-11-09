cda01276909d34af887d90ff796200550a0453f2
mmsesay <maej_01@hotmail.com>
2021-11-09 12:59:54 +0000

    Add: table component and design for the mission component

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Table, Badge, Button } from 'react-bootstrap';
 import getMissionsFromApi from '../redux/api';
 import { getMissionsAction } from '../redux/missions';
 
@@ -11,12 +12,43 @@ export default function Missions() {
       .then((data) => {
         dispatch(getMissionsAction(data));
       })
-      .catch((err) => { throw new Error(err); });
+      .catch((err) => {
+        throw new Error(err);
+      });
   }, []);
 
   return (
-    <div>
-      MISSIONS
+    <div className="px-20 py-30">
+      <Table
+        striped
+        bordered
+        hover
+        className="cursor-pointer"
+        responsive="sm | md | ld | xl"
+      >
+        <thead>
+          <tr className="text-xl">
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-lg">
+            <td className="font-bold">Mark</td>
+            <td>Otto</td>
+            <td><Badge bg="secondary">NOT A MEMBER</Badge></td>
+            <td><Button variant="outline-dark">Join Mission</Button></td>
+          </tr>
+          <tr className="text-lg">
+            <td className="font-bold">Jacob</td>
+            <td>Thornton</td>
+            <td><Badge bg="primary">ACTIVE MEMBER</Badge></td>
+            <td><Button variant="outline-danger">Leave Mission</Button></td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
