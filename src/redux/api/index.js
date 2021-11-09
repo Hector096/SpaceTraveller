fc40d55630c9ab8ad8c04b225b386010a7bc21ca
mmsesay <maej_01@hotmail.com>
2021-11-09 12:21:14 +0000

    Refactor the api to loop through the response data Extract specific key from the object and create a new object Append the object to the array and return

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -3,8 +3,20 @@ import axios from 'axios';
 const baseUrl = 'https://api.spacexdata.com/v3/missions';
 
 const getMissionsFromApi = async () => {
+  const array = [];
   const response = await axios.get(`${baseUrl}`);
-  return response.data;
+
+  response.data.forEach((data) => {
+    const missionObject = {
+      mission_id: data.mission_id,
+      mission_name: data.mission_name,
+      description: data.description,
+    };
+
+    array.push(missionObject);
+  });
+
+  return { data: array };
 };
 
 export default getMissionsFromApi;
